56024ad3a8ee5946a8141d61e4689404fe20e037
NatalyHalouchyk <natallia.halouchyk@gmail.com>
2022-01-11 10:53:00 +0100

    add js page, start running server, parse get request from browser

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+  res.sendFile(__dirname + "/index.html");
+})
+
+
+
+
+app.listen(3000, ()=>{
+  console.log("Server started on port 3000");
+})
